16cd37b63eab428adff9a4d783d77ac628a5eedc
sandokhan <rhaydrick@gmail.com>
2024-12-25 11:00:33 +0000

    restore main file

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de dados")
+
+# Variables
+age = 36
+name = "Sandokhan"
+
+# Print
+print(f"{age}")
+
+# Sum
+
+new_age = age + 1
+print(f"{new_age}")
+
+# List
+lista = [1, 2, 3, 4, 5]
+
+# Methods
+
+lista_invertida = lista.reverse()
